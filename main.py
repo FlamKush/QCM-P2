1f0784c33869ab3e39c82f0599e5ff5cf1230ba7
FlamKush <vdbgaetan03@gmail.com>
2022-10-14 11:06:49 +0200

    Update of main.py
    j'ai ajouter des un premier menu test

--- main.py
+++ main.py
@@ -2,4 +2,11 @@
 
 def menu():
     print("######################################")
+    print("Bienvenue dans le qcm ")
+    print("Pour acceder au cotation classique appuyer sur k")
+    print("Pour acceder a une cotation a points négatifs appuyer sur n")
+    print("######################################")
+    choice = input("=> ")
     
+    
+menu()
